93e6683da2991a6e1b3fb3651536209c1f7ed0fe
Donovan Daniels <hewwo@yiff.rocks>
2022-10-15 02:27:46 -0500

    remove dist

--- dist/getContainerMap.d.ts
+++
@@ -1,2 +0,0 @@
-import Docker from "dockerode";
-export default function getContainerMap(options?: Docker.DockerOptions, keepPeriods?: boolean): Promise<Record<string, string>>;
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as getContainerMap } from "./getContainerMap.js";
-export { default as HostsManager } from "./HostsManager.js";
